7de63a2b0a3db8e5a5d1777dd23b494c067e5793
Djessyka <d.buachack@cinq.com.br>
2020-03-25 22:25:06 -0300

    hotfix

--- src/routes.js
+++ src/routes.js
@@ -11,9 +11,9 @@ routes.post('/sessions', SessionController.create);
 routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.create);
 
-routes.post('/incident', IncidentController.create);
+routes.post('/incidents', IncidentController.create);
 routes.get('/incidents', IncidentController.index);
-routes.delete('/incident/:id', IncidentController.delete);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 routes.get('/profile', ProfileController.index);
 
